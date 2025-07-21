7977110b4e8fe467b5214b06541016cad9cbbd77
alorig <bluesalman@hotmail.com>
2025-07-22 02:15:24 +0500

    more enhance

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title('|', true, 'right'); ?></title>
     <?php wp_head(); ?>
+    <!-- Tom Select CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/css/tom-select.min.css" rel="stylesheet">
+    <!-- Tom Select JS -->
+    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.3.1/dist/js/tom-select.complete.min.js"></script>
 </head>
 <body <?php body_class(); ?>>
     <div class="wrapper"> 
